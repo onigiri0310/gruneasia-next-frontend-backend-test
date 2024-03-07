34f950f5d66799217bd904ed9f56d9576b8e4dbd
onigiri0310 <mi28.sayaka@gmail.com>
2024-03-08 01:26:21 +0900

    add:CompanyCretaePageの機能追加

--- src/pages/company/create/index.page.tsx
+++ src/pages/company/create/index.page.tsx
@@ -1,7 +1,38 @@
-export default function create() {
-    return (
-        <h1>
-            Create Page
-        </h1>
-    );
-}
+import { Button, Card, Title } from "@mantine/core"
+import { type NextPage } from "next"
+import CompanyForm from "../_components/form"
+import NextLink from 'next/link'
+import { FaAngleLeft } from "react-icons/fa"
+import NextHead from 'next/head'
+
+const CompanyCreate: NextPage = () => {
+  return (
+    <>
+      <NextHead>
+        <title>Company Create Page</title>
+      </NextHead>
+      <main className="flex flex-col m-12">
+        <div className="container m-auto">
+          <NextLink href="/user/list">
+              <Button variant="default" color="dark" size="xs" leftIcon={<FaAngleLeft size="1rem" />} className="mb-4">Back</Button>
+          </NextLink>
+          <Card withBorder shadow="sm" radius="sm" >
+            {/* Card Header */}
+            <Card.Section withBorder px="xl" py="sm">
+              <Title order={3} size="text-base" weight="bold" align="left">
+                Create Company
+              </Title>
+            </Card.Section>
+
+            {/* Card Body */}
+            <Card.Section px="xl" py="md">
+              <CompanyForm />
+            </Card.Section>
+          </Card>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default CompanyCreate;
